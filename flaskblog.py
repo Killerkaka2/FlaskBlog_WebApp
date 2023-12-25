75656b136a7883f8c467a6be13aefb9aac45379d
Pranav <pgtambre1@gmail.com>
2023-12-25 17:44:11 +0530

    Routing

--- flaskblog.py
+++ flaskblog.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return "<h1>Hello, World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 
 if __name__== '__main__' :
