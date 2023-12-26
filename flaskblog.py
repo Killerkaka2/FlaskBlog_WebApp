0dcf64666507e08bc9443ebaaea9cc935c00d06e
Pranav <pgtambre1@gmail.com>
2023-12-26 15:13:47 +0530

    Adding Dummy Values

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : "Corey Schafer",
+        'title' : "Blog Post 1",
+        'Comments' : "First Blog Post",
+        'date_posted':"26 April, 2023"
+    },
+    {
+        'author' : "Pranav",
+        'title' : "Blog Post 2",
+        'content' : "Second Blog Post",
+        'date_posted':"20 April, 2018"
+    }
+
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
