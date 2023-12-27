9751699aaa884a1c8479d19069d67a3a772797ed
Pranav <pgtambre1@gmail.com>
2023-12-27 15:04:32 +0530

    Created Form

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2f814d445f3cdb48676288249364de65e4ca91784e1d58de669efcae8ca84390'
 
 posts = [
     {
@@ -28,6 +30,17 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
+@app.route("/register")
+def register():
+    # Creating object (Instance of the Class)
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login")
+def login():
+    # Creating object (Instance of the Class)
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
 
 if __name__== '__main__' :
     app.run(debug=True)
