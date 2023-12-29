ec60d59d19952036d5de8f5955ebffde7fec3467
Pranav <pgtambre1@gmail.com>
2023-12-29 17:41:56 +0530

    Added logout route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User,Post
@@ -57,3 +57,9 @@ def login():
         else:
             flash('Login Failed. Invalid email and Password','danger')
     return render_template("login.html", title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
