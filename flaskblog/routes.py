df1ccd4b1b28c30f1b2511a6b6de9c96cbcabba9
Pranav <pgtambre1@gmail.com>
2023-12-29 17:28:01 +0530

    Hashing done and Login Started

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, flash, redirect
-from flaskblog import app
+from flask_login import current_user, login_user
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User,Post
 
@@ -29,21 +30,30 @@ def about():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated():
+        return redirect(url_for('home'))
     # Creating object (Instance of the Class)
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)   #Creating a new obj/user(Instance of class User)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your Account has been created! You can now Log In','success')
+        return redirect(url_for('login'))
     return render_template("register.html", title='Register', form=form)
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('home'))
     # Creating object (Instance of the Class)
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been Logged In!!','success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Failed. Invalid Username and Password','danger')
+            flash('Login Failed. Invalid email and Password','danger')
     return render_template("login.html", title='Login', form=form)
