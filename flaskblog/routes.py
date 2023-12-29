3d7768b36eebe221b5ceb4197688bc1b8706117a
Pranav <pgtambre1@gmail.com>
2023-12-29 18:34:52 +0530

    Login message category and Next page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for, flash, redirect
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import current_user, login_required, login_user, logout_user
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User,Post
@@ -53,7 +53,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Failed. Invalid email and Password','danger')
     return render_template("login.html", title='Login', form=form)
@@ -63,7 +64,8 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
 # Restriction before logging in
+@app.route("/account")
+@login_required
 def account():
     return render_template('account.html', title='Account')
