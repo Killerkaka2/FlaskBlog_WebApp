a21dcf6338136f194e12c59f1cf34c3fcc8022ba
Pranav <pgtambre1@gmail.com>
2023-12-30 17:40:45 +0530

    Deletion of prev profile pic

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -79,6 +79,10 @@ def save_picture(form_picture):
     i.thumbnail(output_size)
     i.save(picture_path)
     
+    prev_picture = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
+    if os.path.exists(prev_picture):
+        os.remove(prev_picture)
+
     return picture_fn
 
 @app.route("/account", methods=['GET','POST'])
