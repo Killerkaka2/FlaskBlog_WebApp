512409d40fdb50786804a81b3267e60299e484f1
Pranav <pgtambre1@gmail.com>
2024-01-01 13:37:48 +0530

    Front end of New Post form done

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField,PasswordField, SubmitField, BooleanField, ValidationError
+from wtforms import StringField,PasswordField, SubmitField, BooleanField, TextAreaField, ValidationError
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 from flaskblog.models import User
 
@@ -48,3 +48,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('Email already exist. Please choose another one.')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import current_user, login_required, login_user, logout_user
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User,Post
 
 posts = [
@@ -103,3 +103,12 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file = image_file, form=form)
+
+@app.route('/post/new', methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your Post has been Created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form = form)
