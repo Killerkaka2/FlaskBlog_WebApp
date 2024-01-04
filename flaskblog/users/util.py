9412ec614a5791a2b26ca4c781b8c3b2b4b9142d
Pranav <pgtambre1@gmail.com>
2024-01-04 20:19:14 +0530

    Created instance of our application

--- flaskblog/users/util.py
+++ flaskblog/users/util.py
@@ -1,9 +1,9 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_login import current_user
-from flaskblog import app, mail
+from flaskblog import mail
 from flask_mail import Message
 
 
@@ -11,14 +11,14 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 
     output_size = (125,125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
     
-    prev_picture = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
+    prev_picture = os.path.join(current_app.root_path, 'static/profile_pics', current_user.image_file)
     if os.path.exists(prev_picture):
         os.remove(prev_picture)
 
